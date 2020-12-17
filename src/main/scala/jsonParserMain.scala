809178430d675156d738e9dd9612a506d21c22e6
cmorg040 <33102191+cmorg040@users.noreply.github.com>
2020-12-17 12:23:44 -0500

    Stage 2: Now splits JSON values into an array and stores them in a map.

--- src/main/scala/jsonParserMain.scala
+++ src/main/scala/jsonParserMain.scala
@@ -2,13 +2,21 @@ object jsonParserMain extends App{
 
   val file = io.Source.fromFile("dummyData.json")
 
+  val mappedValues = scala.collection.mutable.Map[String,String]()
+
   for (line <- file.getLines) {
 //    println(line)
 //    val brokenLine = line.split(":")
     var strippedLine = line.trim().replaceAll("\"", "").stripSuffix(",")
     // var brokenLine = strippedLine.replaceAll("\"", "")
     // val finishedLine = brokenLine.stripSuffix(",")
-    println(strippedLine)
+    // println(strippedLine)
+
+    val splitLine = strippedLine.split(":")
+    if (splitLine.length > 1) {
+      mappedValues += (splitLine(0) -> splitLine(1))
+    }
+    println(mappedValues)
   }
 
 }
